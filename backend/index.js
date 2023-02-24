a174f5a7aa4e059d196bfb410b41c2f4ce951062
Sincere-Dhairya <varshneydhairya4@gmail.com>
2023-02-24 20:27:42 +0530

    try catch

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ connectToMongoDB();
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json());
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,27 +4,39 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 // Create a user usimg POST "/api/auth/". Doesn't require Auth.
-router.post('/', [
+router.post('/createuser', [
         body('email').isEmail(),
         body('password').isLength({min: 6})
     ],
-    (req, res)=>{
+    async (req, res)=>{
         const errs = validationResult(req);
         if(!errs.isEmpty()){
             return res.status(400).json({errors: errs});
         }
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        }).then(user => res.json(user))
-        .catch(err => {
-            console.log('err', err); 
-            res.json({'errr': err});
-        });
+        
+        try {
+            const user = await User.findOne({email: req.body.email});
+            if(user){
+                console.log('user', user)
+                return res.status(404).json({error : "User with this email already exists!"});
+            }
+            else{
+                await User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                })
+                // .then(user => res.json(user))
+                // .catch(err => {
+                //     console.log('err', err); 
+                //     res.json({'errr': err});
+                // }); 
+                res.json({"result": "User created!"})   
+            }
+        } catch (error) {
+            console.log('error.message', error.message);
+        }
     }
 );
-
-// router.post()
 
 module.exports = router;
