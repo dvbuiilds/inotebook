4eab365ed0af79337521781b7af49e546c9296a0
Sincere-Dhairya <varshneydhairya4@gmail.com>
2023-02-28 22:29:46 +0530

    ading API calls

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -45,35 +45,69 @@ const NoteState = (props) => {
     },
   ];
   const [notes, setNotes] = useState(notesInit);
+  const hostURL = "http://localhost:5000/api/notes/";
+  
+  const getAPIresponse = async(url, data)=>{
+    const response = await fetch(
+      url,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          authToken:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNmYjM4OTM4MTdkZWU3MDVlMmQwNTA4In0sImlhdCI6MTY3NzQwODU5M30.OPEcdAtthMVwYLXr9T2bB7yFwwB3JC65f-pkLGBcYgw",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    return response.json();
+  }
 
   // add a note.
-  const addNote = ({title, description, tag})=>{
+  const addNote = async ({ title, description, tag }) => {
+    const jsonRes = await getAPIresponse(`${hostURL}addnote`, {title, description, tag});
+    console.log('jsonRes', jsonRes);
     // Todo: API call.
     console.log("Adding a new node.");
     const note = {
       _id: makeString(24),
       user: "63fb3893817dee705e2d0508",
       title: title,
-      description:description,
+      description: description,
       tag: tag,
       date: Date.now(),
       __v: 0,
     };
-    setNotes(notes.concat(note))
-  }
+    setNotes(notes.concat(note));
+  };
 
   // delete a note.
-  const deleteNote = (id)=>{
+  const deleteNote = async (id) => {
+    const jsonRes = await getAPIresponse(`${hostURL}deletenote/${id}`, {});
     // console.log('deleting the note with note.id', id);
-    let newNotes = notes.filter((note)=>{return note._id !== id});
+    let newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
     setNotes(newNotes);
-  }
-  
+  };
+
   // update a note.
-  const updateNote = ()=>{}
+  const updateNote = async (id, title, description, tag) => {
+    const jsonRes = await getAPIresponse(`${hostURL}updatenote/${id}`, {});
+    for (let i = 0; i < notes.length; i++) {
+      if (id === notes[i]._id) {
+        notes[i].title = title;
+        notes[i].description = description;
+        notes[i].tag = tag;
+      }
+    }
+  };
 
   return (
-    <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, updateNote }}>
+    <NoteContext.Provider
+      value={{ notes, setNotes, addNote, deleteNote, updateNote }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
@@ -81,8 +115,9 @@ const NoteState = (props) => {
 
 //generates a random string.
 function makeString(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
