1824f757ee2939abee94268f092dbefed47e26e4
Sincere-Dhairya <varshneydhairya4@gmail.com>
2023-02-28 22:31:15 +0530

    adding CORS

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ connectToMongoDB();
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
